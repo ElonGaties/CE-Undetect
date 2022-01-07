3672beefdf3dfdb29a49a84daf27a4ecf7ba0ede
ElonGates <59313695+VilotStar@users.noreply.github.com>
2022-01-07 21:04:54 +0000

    Un retard

--- undetect.py
+++ undetect.py
@@ -1,28 +1,21 @@
 import re
 
-def changeFile(name):
-    in_file = open(name, "rb")
-    data = in_file.read()
-    in_file.close()
+strtorepl = b"Cheat Engine"
+repldastr = b"Check Cached"
 
-    data = data.replace(b"cheatengine", b"checkcached")
-    data = data.replace(b"CheatEngine", b"CheckCached")
-    data = data.replace(b"Cheatengine", b"Checkcached")
-    data = data.replace(b"cheatEngine", b"checkCached")
-    data = data.replace(b"Cheat Engine", b"Check Cached")
-    data = data.replace(b"CHEATENGINE", b"CHECKCACHED")
+def changeFile(fname, strrepl, replstr):
+    f = open(fname, "rb")
+    data = f.read()
+    f.close()
     
-    data = data.replace(b"getCheckCached", b"getCheatEngine")
-    data = data.replace(b"www.checkcached", b"www.cheatengine")
-
-    out_file = open(name, "wb")
-    out_file.write(data)
-    out_file.close()
+    data = data.replace(strrepl, replstr)
     
-    return data
+    f = open(fname, "wb")
+    f.write(data)
+    f.close()
+    
+files = ["cheatengine-x86_64.exe", "cheatengine-i386.exe", "cheatengine-x86_64-SSE4-AVX2.exe"]
 
-files = ["cheatengine-i386.exe", "cheatengine-x86_64.exe"]
-
-for i in files:
-    changeFile(i)
-    print(f"File {i} patched")
+for file in files:
+    changeFile(file, strtorepl, repldastr)
+    print(f"File {file} patched")
